daeeabf6fa4aad4ab89bf60248b98a2501ce8760
Marina Botova <koshkalisa2018@yandex.ru>
2022-11-01 22:14:19 +0300

    add README.md

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1100;
+        int check = 100;
+        boolean isRefill = true;
+
+        int bonus;
+        if (isRefill) {
+            bonus = 11;
+        } else {
+            bonus = 1;
+
+        }
+        int finalBonus = amount / 100;
+        if (finalBonus > 100) {
+            finalBonus = 100;
+        }
+        int balans = amount + finalBonus + check;
+        if (balans > 1100) ;
+
+        {
+
+            System.out.println("Количество бонусов:" + finalBonus);
+            System.out.println("Итоговый счет:" + balans);
+        }
+
+    }
+}
